542c7c1945b526b98e1dc2032adc9c81b3c4cb60
itsajoe <jmusser19@gmail.com>
2017-11-30 18:34:02 -0700

    beautify

--- liri.js
+++ liri.js
@@ -10,11 +10,12 @@ if (!command2) {
 }
 
 var url = "http://www.omdbapi.com/?t=" + movieInput + "&y=&plot=short&apikey=trilogy"
-function movieCall () {
-    request(url, function(error, response, body) {
-        
-          
-          if (!error && response.statusCode === 200) {
+
+function movieCall() {
+    request(url, function (error, response, body) {
+
+
+        if (!error && response.statusCode === 200) {
             var parsed = JSON.parse(body);
             console.log("Movie Title");
             console.log(parsed.Title);
@@ -32,8 +33,8 @@ function movieCall () {
             console.log(parsed.Plot);
             console.log("Movie Actors");
             console.log(parsed.Actors);
-          }
-        });
+        }
+    });
 
 }
 // movieCall();
@@ -44,35 +45,39 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify({
     id: "23e41a94491c43cd98f4001bd9316d3e",
     secret: "ef19d2284ea24f43b6d1b6c7a9adec74"
-  });
-  if (!command2) {
-      var spotifyInput = "The Sign"; 
-  } else {
-      var spotifyInput = command2;
-  }
-  var spotifyInput = "" + spotifyInput + "";
-  function spotifyCall () {
-      spotify
-      .search({ type: 'track', query: spotifyInput })
-      .then(function(response) {
-       var artistNames = response.tracks.items[0].artists; 
-       console.log("The Artists");
-        for (let i = 0; i < artistNames.length; i++) {
-            console.log(artistNames[i].name);
-        }
-        console.log("The Song Name");
-        console.log(response.tracks.items[0].name);
-        console.log("The Preview Url");
-        console.log(response.tracks.items[0].preview_url);
-        console.log("The Album Name");
-        console.log(response.tracks.items[0].album.name);
-        
-    })
-      .catch(function(err) {
-        console.log(err);
-      });
+});
+if (!command2) {
+    var spotifyInput = "The Sign";
+} else {
+    var spotifyInput = command2;
+}
+var spotifyInput = "" + spotifyInput + "";
 
-  };
+function spotifyCall() {
+    spotify
+        .search({
+            type: 'track',
+            query: spotifyInput
+        })
+        .then(function (response) {
+            var artistNames = response.tracks.items[0].artists;
+            console.log("The Artists");
+            for (let i = 0; i < artistNames.length; i++) {
+                console.log(artistNames[i].name);
+            }
+            console.log("The Song Name");
+            console.log(response.tracks.items[0].name);
+            console.log("The Preview Url");
+            console.log(response.tracks.items[0].preview_url);
+            console.log("The Album Name");
+            console.log(response.tracks.items[0].album.name);
+
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+};
 //   spotifyCall();
 //   twitter call
 
@@ -80,25 +85,29 @@ var Twitter = require("twitter");
 var keys = require("./keys.js");
 
 var client = new Twitter(keys);
-var params = {screen_name: "bob_ba_bob"};
-function twitterCall () {
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    for (let i = 0; i < tweets.length; i++) {
-        console.log("Another Tweet");
-        console.log(tweets[i].text);
-        console.log(tweets[i].created_at);
-    }
-    
-  }
-});
+var params = {
+    screen_name: "bob_ba_bob"
+};
+
+function twitterCall() {
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (let i = 0; i < tweets.length; i++) {
+                console.log("Another Tweet");
+                console.log(tweets[i].text);
+                console.log(tweets[i].created_at);
+            }
+
+        }
+    });
 }
 var fs = require("fs");
-function doWhat () {
-    fs.readFile("./random.txt", "utf8", function(err, data) {
+
+function doWhat() {
+    fs.readFile("./random.txt", "utf8", function (err, data) {
         if (err) {
             return console.log(error);
-          }
+        }
         var split = data.split(",");
         spotifyInput = split[1];
         spotifyCall();
@@ -106,17 +115,17 @@ function doWhat () {
 }
 // twitterCall();
 // switch statements
-switch(command){
+switch (command) {
     case "spotify-this-song":
-    spotifyCall();
-    break;
+        spotifyCall();
+        break;
     case "my-tweets":
-    twitterCall();
-    break;
+        twitterCall();
+        break;
     case "movie-this":
-    movieCall();
-    break;
-    case "do-what-it-says": 
-    doWhat();
-    break; 
+        movieCall();
+        break;
+    case "do-what-it-says":
+        doWhat();
+        break;
 }
